d2aee9fa343a0750e96219d64fedc1e2f62ef909
Anu Anujin <anuanujin@Anus-Mac.local>
2025-04-22 19:00:40 -0400

    Level 2 done

---
+++ level_2.py
@@ -0,0 +1,7 @@
+mix_of_stuff = [24, "Mongolia", 3.14, True, [1, 2, 3]]
+
+playlist_for_coding = ["Haru Haru by Big Bang", "The Eternal Vow by Tan Dun", "Airwaves by Pashanim", "Mittelmeer by Pashanim", "Nokia by Drake"]
+
+playlist_for_coding.append("doppel g by Pashanim")
+
+print(playlist_for_coding)
